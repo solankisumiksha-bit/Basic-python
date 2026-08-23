4092bd986030a73c53df40c0db990925e843363b
Sumiksha Solanki <solankisumiksha@gmail.com>
2026-08-23 15:22:01 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,25 @@
+"""
+Module 2: Basic Python Concepts
+ 
+Task 1: Perform Basic Mathematical Operations
+Problem Statement: Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+3.  Displays the results of each operation on the screen.
+"""
+#code 
+
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+addition = a + b
+subtraction = a - b
+multiplication = a * b
+division = a / b
+print("Addition: ", addition) 
+print("Subtraction: ", subtraction)
+print("Multiplication: ", multiplication)
+print("Division: ", division)
---
+++ Task2.py
@@ -0,0 +1,12 @@
+"""
+Task 2: Create a Personalized Greeting
+Problem Statement: Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.
+"""
+#code
+a = input("Enter your first name: ")
+b = input("Enter your last name: ")
+full_name = a + " " + b
+print("Hello, " + full_name + "! Welcome to the program.")
